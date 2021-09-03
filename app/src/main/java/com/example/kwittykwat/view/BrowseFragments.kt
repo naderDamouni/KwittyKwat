bf5b6362ce667f004f9cf1dc4f18056479557d14
Nader Damouni <naderdamouni@naders-air.lan>
2021-09-02 20:01:57 -0400

    Added navigation fragments with functionality

--- app/src/main/java/com/example/kwittykwat/view/BrowseFragments.kt
+++ app/src/main/java/com/example/kwittykwat/view/BrowseFragments.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.kwittykwat.R
 import com.example.kwittykwat.databinding.FragmentBrowseBinding
 
@@ -15,6 +16,13 @@ class BrowseFragments : Fragment(R.layout.fragment_browse) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentBrowseBinding.bind(view)
+
+       val settingsView = binding.browseSettings.setOnClickListener { Navigation.findNavController(view).navigate(R.id.navigateToSettingsFragment) }
+
+       val detailsView = binding.browseDetails.setOnClickListener { Navigation.findNavController(view).navigate(R.id.navigateToDetailsFragment) }
+
+       return settingsView;
+       return detailsView;
     }
 
 }
--- app/src/main/java/com/example/kwittykwat/viewModel/MainActivityViewModel.kt
+++ app/src/main/java/com/example/kwittykwat/viewModel/MainActivityViewModel.kt
@@ -1,9 +1,6 @@
 package com.example.kwittykwat.viewModel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.kwittykwat.model.Kats
 
 class MainActivityViewModel : ViewModel() {
 
